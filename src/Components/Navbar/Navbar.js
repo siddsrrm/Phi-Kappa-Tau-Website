bbc0470d4c1677d16dde5583f0bf0ba51a37ebbd
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-10-25 11:43:06 -0400

    Working on navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import * as styles from './Navbar.module.css'
+import { useState } from 'react'
+
+export const Navbar = () => {
+    const [fix, setFix] = useState(false)
+
+    function setFixed() {
+        if (window.scrollY >= 10) {
+            setFix(true)
+        }
+        else {
+            setFix(false)
+        }
+    }
+
+    window.addEventListener("scroll", setFixed)
+  return (
+    <nav className={fix ? styles.navbarFixed : styles.navbar}> 
+        <ul className={styles.navItems}>
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#experience">Experience</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li> 
+        </ul>            
+    </nav>
+  )
+}
