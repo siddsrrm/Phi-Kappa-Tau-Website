61b6907fe22b4224f07ccad8f1a79a3ab21b3ac5
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2025-01-05 11:57:36 -0500

    Finished chapter facts section

--- src/Components/ChapterFacts/ChapterFacts.js
+++ src/Components/ChapterFacts/ChapterFacts.js
@@ -5,18 +5,22 @@ export const ChapterFacts = () => {
     const facts = [
         {title: 'Founded in', value: '1920'},
         {title: 'Chapter Gpa', value: '3.3'},
-        {title: 'Active Members', value: '100'},
-        {title: 'Alumni Network', value: '500+'},
+        {title: 'Actives', value: '100'},
+        {title: 'Alumni', value: '1000+'},
     ]
   
     return (
-        <div className={styles.factsContainer}>
-            {facts.map((fact, index) => (
-                <div key={index} className={styles.factCircle}>
-                    <div className={styles.factTitle}>{fact.title}</div>
-                    <div className={styles.factValue}>{fact.value}</div>
-                </div>
-            ))}
+        <div>
+            <h1 className={styles.title}>Lambda Chapter</h1>
+            <div className={styles.factsContainer}>
+                {facts.map((fact, index) => (
+                    <div key={index} className={styles.factCircle}>
+                        <div className={styles.factTitle}>{fact.title}</div>
+                        <div className={styles.factValue}>{fact.value}</div>
+                    </div>
+                ))}
+            </div>
         </div>
+        
     )
 }
