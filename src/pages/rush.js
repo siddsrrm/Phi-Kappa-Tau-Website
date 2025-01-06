039bd2e9c787d1ee9d002e4933f45c9013f444e6
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2025-01-06 17:17:12 -0500

    Embedded Rush form

--- src/pages/rush.js
+++ src/pages/rush.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Navbar } from "../Components/Navbar/Navbar"
+import { RushForm } from '../Components/RushForm/RushForm'
+import { Footer } from '../Components/Footer/Footer'
 
 const rush = () => {
   return (
     <div>
         <Navbar />
-        <h1>Rush</h1>
+        <RushForm />
+        <Footer />
     </div>
   )
 }
