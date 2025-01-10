a9563f2456b4a2cb4a473b64be5a7f2ff451a241
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2025-01-10 16:32:40 -0500

    added hamburger menu for navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,79 @@
-import React from 'react'
-import * as styles from './Navbar.module.css'
-import logo from '../../Images/logo.png'
-import instaLogo from '../../Images/instaPng.png'
-import { Link } from 'gatsby'
-
+import React, { useState, useEffect } from 'react';
+import * as styles from './Navbar.module.css';
+import logo from '../../Images/logo.png';
+import instaLogo from '../../Images/instaPng.png';
+import { Link } from 'gatsby';
 
 export const Navbar = () => {
-  return (
-    <nav className={styles.navbar}> 
-        <div className={styles.logoContainer}>
-            <img src={logo} alt='logo' className={styles.logoImg}/>
-            <div>
-                <h1 className={styles.name}>PHI KAPPA TAU</h1>
-                <h1 className={styles.schoolName}>Purdue</h1>
-            </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  // Use useEffect to add or remove the body class for disabling scrolling
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.classList.add(styles.noScroll);
+    } else {
+      document.body.classList.remove(styles.noScroll);
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <nav className={styles.navbar}>
+      {/* Logo Section */}
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="logo" className={styles.logoImg} />
+        <div>
+          <h1 className={styles.name}>PHI KAPPA TAU</h1>
+          <h1 className={styles.schoolName}>Purdue</h1>
         </div>
-        
-        <ul className={styles.navItems}>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/rush">Rush</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li> 
-            <a href="https://www.instagram.com/phitaupurdue/" target="_blank" rel="noopener noreferrer" className={styles.insta}>
-                <img src={instaLogo} alt="Insta icon" className={styles.instaImg}/>
-            </a>
-        </ul>            
+      </div>
+
+      {/* Hamburger Menu for Small Screens */}
+      <button className={styles.hamburger} onClick={toggleMenu} aria-label="Toggle menu">
+        <div className={`${styles.bar} ${isMenuOpen ? styles.barOpen : ''}`}></div>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.barOpen : ''}`}></div>
+        <div className={`${styles.bar} ${isMenuOpen ? styles.barOpen : ''}`}></div>
+      </button>
+
+      {/* Menu Items */}
+      <ul className={`${styles.navItems} ${isMenuOpen ? styles.menuOpen : ''}`}>
+        <li>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/rush" onClick={closeMenu}>
+            Rush
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={closeMenu}>
+            Contact
+          </Link>
+        </li>
+        <a
+          href="https://www.instagram.com/phitaupurdue/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.insta}
+          onClick={closeMenu}
+        >
+          <img src={instaLogo} alt="Instagram icon" className={styles.instaImg} />
+        </a>
+      </ul>
     </nav>
-  )
-}
+  );
+};
