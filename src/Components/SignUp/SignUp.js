350a329bed14cff4a7d31c36e43e5f36516bbc2e
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2025-03-01 20:33:39 -0500

    Implemented sign up functionality

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -3,17 +3,45 @@ import * as styles from './SignUp.module.css';
 
 
 export const SignUp = () => {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Form Data:", formData)
+
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+    });
+  }
+
+
   return (
     <div className={styles.container}>
         <h1 className={styles.title}>Participate in Our Network</h1>
         <h3 className={styles.header}>Sign up with your email to receive semester newsletters and stay updated on alumni events</h3>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <input
             type="text"
             name="firstName"
             placeholder="First Name"
             className={styles.input}
+            value={formData.firstName}
+            onChange={handleChange}
             required
             />
             <input
@@ -21,6 +49,8 @@ export const SignUp = () => {
             name="lastName"
             placeholder="Last Name"
             className={styles.input}
+            value={formData.lastName}
+            onChange={handleChange}
             required
             />
             <input
@@ -28,10 +58,16 @@ export const SignUp = () => {
             name="email"
             placeholder="Email"
             className={styles.input}
+            value={formData.email}
+            onChange={handleChange}
             required
             />
+            <div className={styles.buttonContainer}>
+              <button className={styles.button} type='submit'>Sign up</button>
+            </div>
+            
         </form>
-        <button className={styles.button}>Sign up</button>
+        
     </div>
   )
 }
